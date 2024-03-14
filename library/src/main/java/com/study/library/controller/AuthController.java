7680f418cb6cfe041d503a1445ab1ae9c42ae6d7
kangjy311 <kangjy311@naver.com>
2024-03-14 17:50:25 +0900

    liabrary signup

--- library/src/main/java/com/study/library/controller/AuthController.java
+++ library/src/main/java/com/study/library/controller/AuthController.java
@@ -1,20 +1,42 @@
 package com.study.library.controller;
 
 import com.study.library.aop.annotation.ParamsPrintAspect;
+import com.study.library.aop.annotation.ValidAspect;
 import com.study.library.dto.SignupReqDto;
+import com.study.library.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @ParamsPrintAspect
+    @Autowired
+    private AuthService authService;
+
+    @ValidAspect
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupReqDto signupReqDto) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) {
+        // SignupReqDto 에 어노테이션 달고 @Valid 붙여줘야 됨
+        //@Valid, BindingResult (같이 씀)
 
+        if(authService.isDuplicatedByUsername(signupReqDto.getUsername())) {
+            ObjectError objectError = new FieldError("username", "username", "이미 존재하는 사용자이름입니다.");
+            bindingResult.addError(objectError);
+        }
 
-        return ResponseEntity.ok(null);
+        authService.signup(signupReqDto);
+
+        return ResponseEntity.created(null).body(true);
     }
 
 }
