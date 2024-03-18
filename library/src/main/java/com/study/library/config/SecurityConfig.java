313ebd526434380e4ac97d4c857a97144b7f5517
kangjy311 <kangjy311@naver.com>
2024-03-18 17:33:48 +0900

    library: mypage (jwt,filter)

--- library/src/main/java/com/study/library/config/SecurityConfig.java
+++ library/src/main/java/com/study/library/config/SecurityConfig.java
@@ -1,15 +1,28 @@
 package com.study.library.config;
 
+import com.study.library.security.exception.AuthEntryPoint;
+import com.study.library.security.filter.JwtAuthenticationFilter;
+import com.study.library.security.filter.PermitAllFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PermitAllFilter permitAllFilter;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -18,12 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();    // 없으면 크로스오리진 오류남
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/server/**", "/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .addFilterAfter(permitAllFilter, LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint)
                 ;
     }
 }
