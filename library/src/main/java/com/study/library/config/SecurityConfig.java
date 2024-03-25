e07cfb185a45ea3f29dc91790b29eeae951d8303
kangjy311 <kangjy311@naver.com>
2024-03-25 18:03:43 +0900

    library: (admin 도서관리)

--- library/src/main/java/com/study/library/config/SecurityConfig.java
+++ library/src/main/java/com/study/library/config/SecurityConfig.java
@@ -44,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/mail/authenticate")
                 .permitAll()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
