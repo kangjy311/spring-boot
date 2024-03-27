0d11836d7598b83231d10e55323608679308a80e
kangjy311 <kangjy311@naver.com>
2024-03-27 17:52:25 +0900

    library: (도서관리검색)

--- library/src/main/java/com/study/library/controller/admin/AdminBookController.java
+++ library/src/main/java/com/study/library/controller/admin/AdminBookController.java
@@ -30,10 +30,15 @@ public class AdminBookController {
         return ResponseEntity.created(null).body(true);
     }
 
-    @ParamsPrintAspect
     @GetMapping("/books")
     public ResponseEntity<?> searchBooks(SearchBookReqDto searchBookReqDto) {
 
         return ResponseEntity.ok(bookService.searchBooks(searchBookReqDto));
+    }
+
+    @GetMapping("/books/count")
+    public ResponseEntity<?> getCount(SearchBookReqDto searchBookReqDto) {
+
+        return ResponseEntity.ok(bookService.getBookCount(searchBookReqDto));
     }
 }
--- library/src/main/java/com/study/library/repository/BookMapper.java
+++ library/src/main/java/com/study/library/repository/BookMapper.java
@@ -9,13 +9,18 @@ import java.util.List;
 @Mapper
 public interface BookMapper {
     public int saveBook(Book book);
-    public List<Book> findBooks(
 
+    public List<Book> findBooks(
             @Param("startIndex") int startIndex,
             @Param("count") int count,
             @Param("bookTypeId") int bookTypeId,
             @Param("categoryId") int categoryId,
             @Param("searchTypeId") int searchTypeId,
-            @Param("searchText") String searchText
-            );
+            @Param("searchText") String searchText);
+
+    public int getBookCount(
+            @Param("bookTypeId") int bookTypeId,
+            @Param("categoryId") int categoryId,
+            @Param("searchTypeId") int searchTypeId,
+            @Param("searchText") String searchText);
 }
